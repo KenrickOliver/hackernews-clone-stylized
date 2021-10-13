73ecfc11d8a631fc1008687cc211d2c5cf345c64
KenrickOliver <kenrick.oliver@gmail.com>
2021-10-13 17:58:39 -0400

    fixed navbar styling

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
-import RouterHandler from './router.js'
-
+import RouterHandler from "./router.js";
 
 // START Code for sticky nav
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixedNav)
+const nav = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+window.addEventListener("scroll", fixedNav);
 
 function fixedNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('nav-active')
-    } else {
-        nav.classList.remove('nav-active')
-    }
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("nav-active");
+    logo.classList.add("logo-active");
+  } else {
+    nav.classList.remove("nav-active");
+    logo.classList.remove("logo-active");
+  }
 }
-// END Code for Sticky nav
+//responsive nav
+const toggle = document.querySelector(".toggle__button");
+const navLinks = document.querySelector(".navbar__links");
+const toggleIcon = document.querySelector(".toggle__icon");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  toggleIcon.classList.toggle("bx-menu");
+  toggleIcon.classList.toggle("bx-x");
+});
 
 // Code to check WHICH link is active based on Hash(#) to assign .active class to it
 window.onhashchange = () => {
-    setActiveLink();
-  }
-  
-  function setActiveLink() {
-     const links = document.querySelectorAll('.header-link');
-     links.forEach(link => {
-        const linkPath = link.getAttribute('href');
-        const currentPath = window.location.hash;
-        if (currentPath === linkPath) {
-          link.classList.add('active');  
-        } else {
-          link.classList.remove('active');         
-        }
-     });
-  }
-class App {
-    constructor() {
-        new RouterHandler(); 
-      }  
+  setActiveLink();
+};
+
+function setActiveLink() {
+  const links = document.querySelectorAll(".header-link");
+  links.forEach((link) => {
+    const linkPath = link.getAttribute("href");
+    const currentPath = window.location.hash;
+    if (currentPath === linkPath) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
     }
-    
+  });
+}
+class App {
+  constructor() {
+    new RouterHandler();
+  }
+}
+
 new App();
